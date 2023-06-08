35a60d787aef099708a2324cdc0a71b1783c9b46
Yuri <yuryanurov@gmail.com>
2023-06-08 19:54:01 +0500

    no clients dict

--- main.py
+++ main.py
@@ -2,39 +2,25 @@ from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from starlette.websockets import WebSocketDisconnect
-from typing import Dict
 
-app = FastAPI()
+app = FastAPI(websocket_origin='127.0.0.1')
 templates = Jinja2Templates(directory="templates")
 
-# словарь для хранения подключенных клиентов и их счетчиков
-clients: Dict[WebSocket, int] = {}
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_route(websocket: WebSocket):
     await websocket.accept()
-    
-    # получаем значение счетчика клиента, для нового клиента устанавливаем 1
-    counter = clients.get(websocket, 1)
-    # При перезагрузке страницы отправляем message клиенту
-    if counter == 1:
-        await websocket.send_json({"reset": True})
-        
+    counter = 1
     try:
         while True:
             data = await websocket.receive_json()
             message = {"id": counter, "text": data["message"]}
             await websocket.send_json(message)
             counter += 1
-            clients[websocket] = counter  # обновляем счетчик клиента
     except WebSocketDisconnect:
         pass
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    clients.clear()  # При остановке сервера очищаем словарик клиентов
--- test_app.py
+++ test_app.py
@@ -1,15 +1,21 @@
 from fastapi.testclient import TestClient
 from main import app
 
-def test_websocket_endpoint():
-    client = TestClient(app)
+client = TestClient(app)
 
+
+def test_websocket_message_format():
     with client.websocket_connect("/ws") as websocket:
-        # тестируем первое сообщение с сервера
+        message = {"message": "Test Message"}
+        websocket.send_json(message)
         response = websocket.receive_json()
-        assert "reset" in response
+        assert isinstance(response, dict)
+        assert "id" in response
+        assert "text" in response
 
-        # тестируем отправку сообщений и получение ответов
+
+def test_websocket_send_receive():
+    with client.websocket_connect("/ws") as websocket:
         messages = ["Hello", "World", "Test"]
         for message in messages:
             websocket.send_json({"message": message})
